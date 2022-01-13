b0f8c09cb5570daa8410ff2ef1d0b1769fa292a0
EdwardCabrini <edward.cabrini@nfocus.co.uk>
2022-01-13 14:05:16 +0000

    POM patch 2

--- nfocus Project/uk.co.edgewordstraining@nfocus.edward.cabrini/POM's/LoginPOM.cs
+++ nfocus Project/uk.co.edgewordstraining@nfocus.edward.cabrini/POM's/LoginPOM.cs
@@ -13,23 +13,29 @@ namespace uk.co.edgewordstraining.nfocus.edward.cabrini.POM_s
 {
     public class LoginPOM
     {
+        //Instatiate driver
         private IWebDriver driver;
 
         public LoginPOM(IWebDriver driver)
         {
             this.driver = driver;
         }
+        //Paramaterise web elements
         public IWebElement UsernameField => driver.FindElement(By.CssSelector("input#username"));
         public IWebElement PasswordField => driver.FindElement(By.CssSelector("input#password"));
         public IWebElement Submit => driver.FindElement(By.CssSelector("button[name='login']"));
         public LoginPOM Login (String Username, String Password)
         {
+            //Send commands to driver
+            //Insert fresh username
             UsernameField.Click();
             UsernameField.Clear();
             UsernameField.SendKeys(Username);
+            //Insert fresh password
             PasswordField.Click();
             PasswordField.Clear();
             PasswordField.SendKeys(Password);
+            //Click login button
             Submit.Click();
             return this;
         }
